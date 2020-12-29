444e0477d43667f4f7d469875449d79ed8e9cb7c
Windom Wu <drw_08@hotmail.com>
2020-12-29 10:37:10 +0000

    add user model and service in SPA

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
+import { UserService } from './_services/user.service';
 
 
 @NgModule({
@@ -45,6 +46,7 @@ import { AuthGuard } from './_guards/auth.guard';
     ErrorInterceptorProvider,
     AlertifyService,
     AuthGuard,
+    UserService,
   ],
   bootstrap: [AppComponent]
 })
