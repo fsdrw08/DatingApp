683f9111f02d45916775b9a395b608d1f23de587
Windom WU <drw_08@hotmail.com>
2021-03-07 16:10:23 +0000

    config ng2-file-uploader 1

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -11,38 +11,28 @@ import { environment } from 'src/environments/environment';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() photos: Photo[];
-  
-  uploader:FileUploader;
-  hasBaseDropZoneOver:boolean;
+
+  uploader: FileUploader;
+  hasBaseDropZoneOver: boolean;
   baseUrl = environment.apiUrl;
 
-  constructor(private authService: AuthService) { 
+  constructor(private authService: AuthService) {
     this.uploader = new FileUploader({
-      url: this.baseUrl + 'user/' + this.authService.decodedToken.nameid + '/photos',
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
       authToken: 'Bearer ' + localStorage.getItem('token'),
       isHTML5: true,
       allowedFileType: ['image'],
       removeAfterUpload: true,
       autoUpload: false,
       maxFileSize: 10*1024*1024,
-      disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
-      formatDataFunctionIsAsync: true,
-      formatDataFunction: async (item) => {
-        return new Promise( (resolve, reject) => {
-          resolve({
-            name: item._file.name,
-            length: item._file.size,
-            contentType: item._file.type,
-            date: new Date()
-          });
-        });
-      }
     });
-
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
     this.hasBaseDropZoneOver = false;
   }
 
-  public fileOverBase(e:any):void {
+  public fileOverBase(e: any): void {
     this.hasBaseDropZoneOver = e;
   }
 
