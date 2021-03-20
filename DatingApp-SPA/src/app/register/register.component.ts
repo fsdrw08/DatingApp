3db7b67de92a901ef7bfb37e52f0179e4d56bda0
Windom WU <drw_08@hotmail.com>
2021-03-20 03:26:31 +0000

    use formgroup instead of ngform, and create custom validator

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 
@@ -10,20 +11,34 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
   @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup;
 
   model: any = {};
 
   constructor(private authService: AuthService, private alertifyService: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required),
+    }, this.passwordConfrimMatcher);
+  }
+
+  passwordConfrimMatcher(formGroup: FormGroup) {
+    return formGroup.get('password').value === formGroup.get('confirmPassword').value ? null : {mismatch : true};
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertifyService.success('registration success');
-    }, error => {
-      this.alertifyService.error(error);
-    });
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertifyService.success('registration success');
+    // }, error => {
+    //   this.alertifyService.error(error);
+    // });
+    console.log(this.registerForm);
   }
 
   cancel() {
