6315a923208022e728d39b76666eaea9efd9eca7
Windom Wu <drw_08@hotmail.com>
2021-04-07 10:01:48 +0000

    try with xunit test

---
+++ DatingApp.API.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using DatingApp.API.Controllers;
+using Xunit;
+
+namespace DatingApp.API.Tests
+{
+    public class UnitTest1
+    {
+        // hard to test thick controller 
+        // https://andrewlock.net/should-you-unit-test-controllers-in-aspnetcore/
+        //ValuesController valuesController = new ValuesController(logger, context);
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
